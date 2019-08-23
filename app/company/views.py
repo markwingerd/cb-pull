243cead5a176b89c8b3d13fc38b9f712310a56c2
Mark Wingerd <mawingerd@gmail.com>
2019-08-23 15:35:36 -0700

    Add company views

--- app/company/views.py
+++ app/company/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
+from .models import Company
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the company index.")
+class IndexView(generic.ListView):
+    template_name = 'company/index.html'
+    context_object_name = 'company_list'
+
+    def get_queryset(self):
+        return Company.objects.order_by('name')
+
+class DetailView(generic.DetailView):
+    model = Company
+    template_name = 'company/detail.html'
